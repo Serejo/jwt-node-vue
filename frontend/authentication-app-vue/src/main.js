e03ebb9a31cd3a5b70c6c4ea4480215f8e387305
Erick Serejo <erickrainier@hotmail.com>
2023-04-05 15:39:06 -0300

    vuelidate

--- frontend/authentication-app-vue/src/main.js
+++ frontend/authentication-app-vue/src/main.js
@@ -1,4 +1,6 @@
 import Vue from "vue";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import Vuelidate from "vuelidate";
 
 import App from "./App.vue";
 import router from "./router";
@@ -6,7 +8,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import "nprogress/nprogress.css";
 
-// Vue.use();
+Vue.use(Vuelidate);
 
 Vue.config.productionTip = false;
 
